6c2605b16e2584e7db21941214282531e5370255
alejodosr <alejandro.rramos@alumnos.upm.es>
2021-04-07 21:34:30 +0100

    improved cats

--- nfts-utils/scrap_cryptokitties_web.py
+++ nfts-utils/scrap_cryptokitties_web.py
@@ -18,49 +18,76 @@ def convert_with_cairosvg_sizes(file_svg, file_png):
             )
 
 
-DATASET_FOLDER = '/media/alejandro/TOSHIBA EXT/ale/ai_kitties_90000'
-DATASET_SIZE = 50000
+DATASET_FOLDER = '/media/alejandro/TOSHIBA EXT/ale/ai_kitties_50000'
+DATASET_SIZE = 70000
+MAX_NUMBER_OF_GENERATIONS = 30
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 
-url = "https://www.cryptokitties.co/search/"
+url = "https://www.cryptokitties.co/search?search=gen:"
 
 # Initiate the browser
 browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 
 # Number of images
-imgs_num = 4007
-start = imgs_num // 12
+total_imgs_num = 0
+total_imgs_repeated = 0
+start = (total_imgs_num + total_imgs_repeated) // 12
+start_gen = (start // (DATASET_SIZE // (12 * MAX_NUMBER_OF_GENERATIONS)) + 1)
+start = start // start_gen
+print(start_gen)
+print(start)
+print(DATASET_SIZE // (12 * MAX_NUMBER_OF_GENERATIONS))
 
-for i in range(start, DATASET_SIZE // 12):
-    # Current numner of images
-    print("INFO: Current number of images " + str(imgs_num))
-
+for gen in range(start_gen, MAX_NUMBER_OF_GENERATIONS):
     # Page number
-    browser.get(url + str(i))
-    print("INFO: Current url " + url + str(i))
+    browser.get(url + str(gen))
 
-    while len(browser.find_elements_by_tag_name('img')) <= 3:
-        time.sleep(1)
-        print("INFO: waiting to load page...")
+    # Number of images per gen
+    imgs_num = 0
 
-    imgs = browser.find_elements_by_tag_name('img')
-    for img in imgs:
-        try:
-            print("INFO: raw image svg " + str(img.get_attribute('src')))
-            svg_filename = str(wget.download(img.get_attribute('src'), out=DATASET_FOLDER))
+    while imgs_num < (DATASET_SIZE // MAX_NUMBER_OF_GENERATIONS):
+        # Current numner of images
+        print("INFO: Current url " + url + str(gen))
+        print("INFO: Generation " + str(gen))
+        print("INFO: Current number of images " + str(total_imgs_num))
+        print("INFO: Current redundant images " + str(total_imgs_repeated))
+        print("INFO: Percentage of redundant images " + str(int(total_imgs_repeated / (1e-9 + total_imgs_num) * 100)) + "%")
+        print("INFO: Target number of images per generation " + str(DATASET_SIZE // MAX_NUMBER_OF_GENERATIONS))
 
-            convert_with_cairosvg_sizes(svg_filename, os.path.join(DATASET_FOLDER, svg_filename.split('/')[-1].split('.')[0] + ".png"))
+        imgs = browser.find_elements_by_tag_name('img')
+        for img in imgs:
+            try:
+                print("INFO: raw image svg " + str(img.get_attribute('src')))
+                svg_filename = str(wget.download(img.get_attribute('src'), out=DATASET_FOLDER))
 
-            os.system("rm " + '"' + svg_filename + '"')
+                if os.path.exists(os.path.join(DATASET_FOLDER, svg_filename.split('/')[-1].split('.')[0] + ".png")):
+                    print("\nINFO: Redundant file: " + os.path.join(DATASET_FOLDER, svg_filename.split('/')[-1].split('.')[0] + ".png"))
+                    # convert_with_cairosvg_sizes(svg_filename, os.path.join(DATASET_FOLDER,
+                    #                                                        svg_filename.split('/')[-1].split('.')[
+                    #                                                            0] + "_copy.png"))
+                    total_imgs_repeated += 1
 
-            # Increase the number of images
-            imgs_num += 1
-        except Exception as e:
-            print("WARNING: Forbidden image")
-            print(e)
+                else:
+                    convert_with_cairosvg_sizes(svg_filename, os.path.join(DATASET_FOLDER, svg_filename.split('/')[-1].split('.')[0] + ".png"))
+                    # Increase the number of images
+                    total_imgs_num += 1
+                    imgs_num += 1
 
-    time.sleep(2)
+                os.system("rm " + '"' + svg_filename + '"')
+
+            except Exception as e:
+                print("WARNING: Forbidden image")
+                print(e)
+
+        button = browser.find_elements_by_class_name('Pagination-button')[1]
+        button.click()
+
+        while len(browser.find_elements_by_class_name('Loader')):
+            time.sleep(1)
+            print("INFO: waiting to load page...")
+
+        time.sleep(2)
 
 browser.quit()
